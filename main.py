f923720094e8ffa47124b7b310197becb90d66de
SageMar <115049261+SageMar@users.noreply.github.com>
2025-02-19 15:11:20 -0800

    Added a submit button and lined them up

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
 
         self.setWindowTitle("AI Grader")
-
+        
         # center widget
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
 
 
         #layout for widget
-        layout = QtWidgets.QVBoxLayout()
+        layout = QtWidgets.QHBoxLayout()
         central_widget.setLayout(layout)
 
-
+        # align the buttons
         button = QtWidgets.QPushButton("Upload")
         layout.addWidget(button)
+        button2 = QtWidgets.QPushButton("Submit")
+        layout.addWidget(button2)
 
         layout.setAlignment(QtCore.Qt.AlignCenter)
+        self.resize(500, 300)
 
 
 app = QtWidgets.QApplication(sys.argv)
