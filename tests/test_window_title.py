7b4723d43526cefb9a2792b08a08da064477271d
ejDeister <ethandeister@gmail.com>
2025-02-27 11:29:33 -0800

    Restructured project, added 'test data', restructured main.py, fixed imports.

--- tests/test_window_title.py
+++ tests/test_window_title.py
@@ -1,18 +1,18 @@
 from main import MainWindow
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QTimer
 import sys
 
 import unittest
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
+window.setWindowTitle("WIP")
 window.show()
 
 
 class test(unittest.TestCase):
     def test_window_title_name(self):
-        self.assertEqual(window.windowTitle(), "AI Grader")
+        self.assertEqual(window.windowTitle(), "WIP")
 
 
 if __name__ == "__main__":
