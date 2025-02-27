3e04fb6680054ef2038e86d2f917f23cecda8242
ejDeister <ethandeister@gmail.com>
2025-02-26 19:42:12 -0800

    Added back window.close() but with app.exec_() to process it

--- tests/test_window_title.py
+++ tests/test_window_title.py
@@ -12,7 +12,8 @@ window.show()
 class test(unittest.TestCase):
     def test_window_title_name(self):
         self.assertEqual(window.windowTitle(), "AI Grader")
-        sys.exit(app.exec_())
+        window.close()
+        app.exec_()
 
 if __name__ == "__main__":
     unittest.main()
