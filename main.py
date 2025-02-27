57028c55a0f52f5f150c14c656f73221dabfb493
ejDeister <ethandeister@gmail.com>
2025-02-26 19:04:00 -0800

    Simplified imports in main.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore
 
 import sys
 
@@ -6,28 +6,28 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 
 from ai_client import get_ai_response
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("AI Grader")
         
         # center widget
-        central_widget = QtWidgets.QWidget()
+        central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
 
         #layout for widget
-        layout = QtWidgets.QHBoxLayout()
+        layout = QHBoxLayout()
         central_widget.setLayout(layout)
 
 
         # align the buttons
-        button = QtWidgets.QPushButton("Upload")
+        button = QPushButton("Upload")
         layout.addWidget(button)
-        button2 = QtWidgets.QPushButton("Submit")
+        button2 = QPushButton("Submit")
         layout.addWidget(button2)
-        button3 = QtWidgets.QPushButton("Ask AI")
+        button3 = QPushButton("Ask AI")
         button3.clicked.connect(onClickAI)
         layout.addWidget(button3)
 
@@ -38,7 +38,7 @@ def onClickAI():
     print("Clicked")
     print(get_ai_response("Give this sentence a score A to F looking for any mistakes: 'How doot you do?'"))
 
-app = QtWidgets.QApplication(sys.argv)
+app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
