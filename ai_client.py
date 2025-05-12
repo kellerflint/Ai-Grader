d7fcdee84c851688d21bd7af2bab3071edccd4eb
cmatthews444 <155667279+cmatthews444@users.noreply.github.com>
2025-05-12 15:54:03 -0700

    changed ai_client to fit openrouter requirments, following https://openrouter.ai/docs/api-reference/chat-completion , swapped from pythons sdk to sending post requests using the request library

--- ai_client.py
+++ ai_client.py
@@ -1,60 +1,65 @@
+import json, os
 import default_settings as default
+import requests
 from dotenv import load_dotenv
-from groq import Groq
 from api_key_functions import get_api_key
-
+model = os.getenv("OPENROUTER_MODEL","meta-llama/llama-3-70b-instruct")
 # Returns an ai client object
 def ai_client():
     api_key=get_api_key()
-    client = Groq(api_key=api_key)
     if not api_key:
         raise ValueError("API_KEY not found in .env file. Please add it.")
-    return client
+    return api_key
 
 def get_ai_response(user_input: str):
-    client = ai_client()
-    print(client)
+    api_key = ai_client()
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model" : model,
+        "messages": [
+            {"role": "system", "content": default.PROMPT},
+            {"role": "user", "content": user_input}
+        ]
+    }
     try:
-        #print(default.PROMPT)
-        #print(user_input)
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": default.PROMPT
-                },
-                {
-                    "role": "user",
-                    "content": user_input,
-                }
-            ],
-            model="llama-3.3-70b-versatile",
+        response = requests.post(
+            "https://openrouter.ai/api/v1/chat/completions",
+            headers=headers,
+            json=data
         )
-        return chat_completion.choices[0].message.content
-    except:
-        return "Error: No response. Check Settings to update your API key"
-    
+        response.raise_for_status()
+        completions = response.json()
+        return completions["choices"][0]["message"]["content"]
+    except requests.exceptions.RequestException as error:
+        return f"Error: {error}"
+
+
 # Very similar to get_ai_response, except it works with the aggregate function
 # TODO: Probably make it one function instead
 def get_ai_response_2(system_prompt, user_prompt: str):
-    client = ai_client()
-    print(client)
+    api_key = ai_client()
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model" : model,
+        "messages": [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    }
     try:
-        print(system_prompt)
-        print(user_prompt)
-        chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": system_prompt
-                },
-                {
-                    "role": "user",
-                    "content": user_prompt,
-                }
-            ],
-            model="llama-3.3-70b-versatile",
+        response = requests.post(
+            "https://openrouter.ai/api/v1/chat/completions",
+            headers=headers,
+            json=data
         )
-        return chat_completion.choices[0].message.content
-    except:
-        return "Error: No response. Check Settings to update your API key"
+        response.raise_for_status()
+        completions = response.json()
+        return completions["choices"][0]["message"]["content"]
+    except requests.exceptions.RequestException as error:
+        return f"Error: {error}"
